bfbd83a8d0a4b4e18e4e634ae264b8aa7b3852ac
Fong Chun Chan <fongchunchan@gmail.com>
2015-07-14 11:19:02 -0700

    Updating Documentation

--- R/get_TSS_window.R
+++ R/get_TSS_window.R
@@ -1,26 +1,27 @@
-#' Return the interval 
+#' Return the interval.
 #' 
-#' This function will return a window of upstream and downstream 
-#' of every transcription start site in the snpeff.dt. 
+#' This function will return a window that is upstream and downstream
+#' of every transcription start site in the snpeff.dt object.
 #'
 #' This function takes into account the strand the gene is on. So a 
 #' downstream.len will apply to strand directionality the gene is.
 #' For instance, if gene A is on the - strand then the downstream.len applies
-#' to the end coordinate of the gene. 
+#' "upstream" to the end coordinate of the gene (i.e. final downstream will
+#' be less than the TSS site). 
 #
-#' The upstream and downstream coordinate columns are reported from a 5' 
+#' The final upstream and downstream coordinate columns are reported from a 5' 
 #' respect regardless of what strand the gene is one (similar to how gene 
-#' coordinates reported in general)
+#' coordinates reported in general).
 #' 
 #' @param snpeff.dt SnpEff database dump data.table loaded through the 
-#'        load_snpeff_db_dump() function
+#'        load_snpeff_db_dump() function.
 #' @param upstream.len Integer value indicating the number of bases to include 
-#'        upstream of the transcription start site. 
+#'        upstream of the transcription start site.
 #' @param downstream.len Integer value indicating the number of bases to include 
-#'        downstream of the transcription start site
+#'        downstream of the transcription start site.
 #'        
-#' @return A data.table of contain the chr, upstream, downstream, and geneName
-#'         of the around the TSS region
+#' @return A data.table of containing the chr, upstream, downstream, and 
+#'         geneName of the around the TSS region.
 #' @export
 get_TSS_window <- function(snpeff.dt, upstream.len = 0, downstream.len = 2000) {
 
